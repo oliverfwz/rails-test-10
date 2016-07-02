f0505f1dde70d7321add24082e3781e4b73fe7db
Oliver Futureworkz <oliver@futureworkz.com>
2016-07-02 09:54:53 +0700

    Create comment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702024420) do
+ActiveRecord::Schema.define(version: 20160702024754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text    "message"
+    t.integer "user_id"
+    t.integer "parent_id"
+  end
+
+  add_index "comments", ["parent_id"], name: "index_comments_on_parent_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
